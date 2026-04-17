af0fe7441620c5ba75043365054f2fd39cb1878e
Hendrix Mycole <62733144+C0L5@users.noreply.github.com>
2026-04-18 00:47:26 +1000

    Updated Content

--- expandImage.js
+++
@@ -1,7 +0,0 @@
-function openImage(img) {
-            const modalImg = document.getElementById("modalImage");
-            modalImg.src = img.src;
-
-            const modal = new bootstrap.Modal(document.getElementById('imageModal'));
-            modal.show();
-        }
